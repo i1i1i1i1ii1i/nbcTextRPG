4d3bf66799fd4417e786bb70661ba6d457d82ff2
euny0ng <h_lyn@hanmail.net>
2025-07-11 00:19:43 +0900

    Creat ItemDB

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
 
 namespace nbcTextRPG;
 
@@ -9,30 +10,131 @@ class Program
         MainMenu,
         Status,
         Inventory,
-        Shop
+        Shop,
+        Exit,
+        EquipInventory,
+        BuyShop
+    }
+
+    public class ItemDB
+    {
+        public List<Item> AllItems = new List<Item>
+        {
+            new Item {
+                ItemNum = 1,
+                ItemName = "수련자 갑옷",
+                ItemType="Armor",
+                ItemDef=5,
+                ItemInfo="수련에 도움을 주는 갑옷입니다."
+            },
+            new Item {
+                ItemNum = 2,
+                ItemName = "무쇠갑옷",
+                ItemType="Armor",
+                ItemDef=9,
+                ItemInfo="무쇠로 만들어져 튼튼한 갑옷입니다."
+            },
+            new Item {
+                ItemNum = 3,
+                ItemName = "스파르타의 갑옷",
+                ItemType="Armor",
+                ItemDef=15,
+                ItemInfo="스파르타의 전사들이 사용했다는 전설의 갑옷입니다."
+            },
+            new Item {
+                ItemNum = 4,
+                ItemName = "낡은 검",
+                ItemType="Weapon",
+                ItemAtk=2,
+                ItemInfo="쉽게 볼 수 있는 낡은 검 입니다."
+            },
+            new Item {
+                ItemNum = 5,
+                ItemName = "청동 도끼",
+                ItemType="Weapon",
+                ItemAtk=5,
+                ItemInfo="어디선가 사용됐던거 같은 도끼입니다."
+            },
+            new Item {
+                ItemNum = 6,
+                ItemName = "스파르타의 창",
+                ItemType="Weapon",
+                ItemAtk=7,
+                ItemInfo="스파르타의 전사들이 사용했다는 전설의 창입니다."
+            }
+        };
+    }
+
+    public class Player
+    {
+        public int PlayerLv { get; set; }
+        public string PlayerName { get; set; }
+        public string PlayerJob { get; set; }
+        public int PlayerAtk { get; set; }
+        public int PlayerDef { get; set; }
+        public int PlayerHP { get; set; }
+        public int PlayerGold { get; set; }
+
+        public void PlayerStatus()
+        {
+            Console.WriteLine($"LV. {PlayerLv}");
+            Console.WriteLine($"{PlayerName} ( {PlayerJob} )");
+            Console.WriteLine($"공격력 : {PlayerAtk}");
+            Console.WriteLine($"방어력 : {PlayerDef}");
+            Console.WriteLine($"체  력 : {PlayerHP}");
+            Console.WriteLine($"소지금 : {PlayerGold}G");
+
+        }
+    }
+
+    public class Item
+    {
+        public int ItemNum { get; set; }
+        public string ItemName { get; set; }
+        public string ItemType { get; set; }
+        public int ItemAtk { get; set; }
+        public int ItemDef { get; set; }
+        public string ItemInfo { get; set; }
+        public int ItemGold { get; set; }
+
+        public void ItemStatus()
+        {
+            if (ItemType == "Weapon")
+            {
+                Console.WriteLine($"{ItemName} | 공격력 : {ItemAtk} | {ItemInfo} | {ItemGold} G");
+            }
+            else if (ItemType == "Armor")
+            {
+                Console.WriteLine($"{ItemName} | 방어력 : {ItemDef} | {ItemInfo} | {ItemGold} G");
+            }
+        }
     }
 
     static void Main(string[] args)
     {
         Page currentPage = Page.MainMenu;
+        bool onOff = true;
+        Player player = new Player();
+        ItemDB itemDB = new ItemDB();
 
-
-        while (true)
+        while (onOff)
         {
             switch (currentPage)
             {
                 case Page.MainMenu:
+                    // Console.Clear();
                     Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다");
                     Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다");
                     Console.WriteLine();
                     Console.WriteLine("1. 상태 보기");
                     Console.WriteLine("2. 인벤토리");
                     Console.WriteLine("3. 상점");
+                    Console.WriteLine("4. 게임종료");
                     Console.WriteLine();
                     Console.WriteLine("원하시는 행동을 입력해주세요.");
                     Console.Write(">>");
                     int pageNumMain = int.Parse(Console.ReadLine());
-                    if (1 <= pageNumMain && pageNumMain <= 3)
+                    if (1 <= pageNumMain && pageNumMain <= 4)
                     {
                         currentPage = (Page)pageNumMain;
                     }
@@ -42,24 +144,28 @@ class Program
                     }
                     break;
                 case Page.Status:
+                    //Console.Clear();
                     Console.WriteLine("상태 보기");
                     Console.WriteLine("캐릭터의 정보가 표시됩니다.");
                     Console.WriteLine();
-                    Console.WriteLine("Lv. 01");
-                    Console.WriteLine("Chad ( 전사 )");
-                    Console.WriteLine("공격력 : 10");
-                    Console.WriteLine("방어력 : 5");
-                    Console.WriteLine("체  력 : 100");
-                    Console.WriteLine("Gold : 1500G");
+                    player.PlayerStatus();
                     Console.WriteLine();
                     Console.WriteLine("0. 나가기");
                     Console.WriteLine();
                     Console.WriteLine("원하시는 행동을 입력해주세요.");
                     Console.Write(">>");
                     int pageNumStat = int.Parse(Console.ReadLine());
-                    currentPage = (Page)pageNumStat;
+                    if (pageNumStat == 0)
+                    {
+                        currentPage = (Page)pageNumStat;
+                    }
+                    else
+                    {
+                        Console.WriteLine("잘못된 입력입니다");
+                    }
                     break;
                 case Page.Inventory:
+                    //Console.Clear();
                     Console.WriteLine("인벤토리");
                     Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
                     Console.WriteLine();
@@ -71,9 +177,21 @@ class Program
                     Console.WriteLine("원하시는 행동을 입력해주세요.");
                     Console.Write(">>");
                     int pageNumItem = int.Parse(Console.ReadLine());
-                    currentPage = (Page)pageNumItem;
+                    if (pageNumItem == 0)
+                    {
+                        currentPage = (Page)pageNumItem;
+                    }
+                    else if (pageNumItem == 1)
+                    {
+                        currentPage = Page.EquipInventory;
+                    }
+                    else
+                    {
+                        Console.WriteLine("잘못된 입력입니다");
+                    }
                     break;
                 case Page.Shop:
+                    //Console.Clear();
                     Console.WriteLine("상점");
                     Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.");
                     Console.WriteLine();
@@ -81,12 +199,10 @@ class Program
                     Console.WriteLine("800 G");
                     Console.WriteLine();
                     Console.WriteLine("[아이템 목록]");
-                    Console.WriteLine("1");
-                    Console.WriteLine("2");
-                    Console.WriteLine("3");
-                    Console.WriteLine("4");
-                    Console.WriteLine("5");
-                    Console.WriteLine("6");
+                    foreach (Item item in itemDB.AllItems)
+                    {
+                        item.ItemStatus();
+                    }
                     Console.WriteLine();
                     Console.WriteLine("1. 아이템 구매");
                     Console.WriteLine("0. 나가기");
@@ -94,7 +210,29 @@ class Program
                     Console.WriteLine("원하시는 행동을 입력해주세요.");
                     Console.Write(">>");
                     int pageNumShop = int.Parse(Console.ReadLine());
-                    currentPage = (Page)pageNumShop;
+                    if (pageNumShop == 0)
+                    {
+                        currentPage = (Page)pageNumShop;
+                    }
+                    else
+                    {
+                        Console.WriteLine("잘못된 입력입니다");
+                    }
+                    break;
+                case Page.EquipInventory:
+                    Console.WriteLine("인벤토리");
+                    Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.");
+                    Console.WriteLine();
+                    Console.WriteLine("[아이템 목록]");
+                    Console.WriteLine();
+                    Console.WriteLine("1. 장착 관리");
+                    Console.WriteLine("0. 나가기");
+                    Console.WriteLine();
+                    Console.WriteLine("원하시는 행동을 입력해주세요.");
+                    Console.Write(">>");
+                    break;
+                case Page.Exit:
+                    onOff = false;
                     break;
             }
         }
--- obj/Debug/net9.0/nbcTextRPG.AssemblyInfo.cs
+++ obj/Debug/net9.0/nbcTextRPG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nbcTextRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+709919dc1e1e64e134dca98cae9a3ed41550850a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6167813c7f35cc37b17f1e1559b321a84eb539bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("nbcTextRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nbcTextRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
